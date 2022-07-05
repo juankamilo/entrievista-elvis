95e6a50fd8e74e7b2ff12431c885801ae35c7260
Elvis Gannem <egannem@gmail.com>
2022-07-05 15:53:29 -0300

    Sending matchday data to view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SearchHistory;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function getIndex()
     {
-        return response()->view('index');
+        $search = SearchHistory::all();
+
+        $array_result = array();
+        foreach ($search as $key=>$item) {
+            $array_result[$key]['id'] = $item->id;
+            $array_result[$key]['competition_id'] = $item->competition_id;
+            $array_result[$key]['created_at'] = $item->created_at;
+            $array_result[$key]['competition_name'] = $item->competition_name;
+            $array_result[$key]['winner'] = $item->winner;
+            $array_result[$key]['matchday'] = $item->matchday;
+        }
+
+        return response()->view('index', [ 'array_result' => $array_result ]);
     }
 }
