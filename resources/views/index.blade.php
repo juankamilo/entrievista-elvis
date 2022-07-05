acc49b078eaa2d14b343d0d565069ec7691c97d0
Elvis Gannem <egannem@gmail.com>
2022-07-05 15:53:38 -0300

    Sending matchday data to view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,31 +14,51 @@
 
         <form action="{{ route('search') }}" method="POST">
             <select name="select-competition" class="form-control mb-4">
-                <option value="CL">Champions League</option>
-                <option value="PL">Premier League</option>
+                <option value="CL" name="Champions League">Champions League</option>
+                <option value="PL" name="Premier League">Premier League</option>
             </select>
 
-            <input type="submit" class="btn btn-primary w-100 mb-5" value="Buscar" />
+            <div class="d-flex gap-3 mb-5">
+                <input type="number" name="matchday" class="form-control w-50" placeholder="Dia del partido (dejar vacio para buscar todo el mes)">
+                <input type="submit" class="btn btn-primary w-50" value="Buscar" />
+            </div>
         </form>
 
+        @if(!empty($array_result))
         <h2>Busquedas recientes</h2>
         <table class="table">
             <thead>
                 <tr>
                     <th scope="row">#</th>
                     <th scope="row">Fecha</th>
-                    <th scope="col">Competición</th>
-                    <th scope="col">Acción</th>
+                    <th scope="col">ID Competición</th>
+                    <th scope="col">Nombre de la competición</th>
+                    <th scope="col">Ganador</th>
+                    <th scope="col">Dia del partido</th>
                 </tr>
             </thead>
 
             <tbody>
-                <th scope="row">1</th>
-                <td>01-01-2020</td>
-                <td>Bundesliga</td>
-                <td><button class="btn btn-success">Buscar nuevamente</button></td>
+            @foreach($array_result as $result)
+                <tr>
+                    <th scope="row">{{ $result['id'] }}</th>
+                    <td>{{ $result['created_at'] }}</td>
+                    <td>{{ $result['competition_id'] }}</td>
+                    <td>{{ $result['competition_name'] }}</td>
+                    <td>{{ $result['winner'] }}</td>
+                    <td>{{ isset($result['matchday']) ? $result['matchday'] : 'Busqueda General' }}</td>
+                </tr>
+            @endforeach
             </tbody>
         </table>
+
+        @else
+            @if(isset($error))
+                <p class="text-danger">{{ $error }}</p>
+            @else
+                <h3>No hay ninguna busqueda reciente</h3>
+            @endif
+        @endif
     </div>
 
     </body>
