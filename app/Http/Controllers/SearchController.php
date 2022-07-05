a944243e2393151aeb0eaf0a3845620ed092ae09
Elvis Gannem <egannem@gmail.com>
2022-07-05 14:56:49 -0300

    Refactoring search method

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,12 +16,18 @@ class SearchController extends Controller
            'X-Auth-Token' => '709dea6acbc04fc2adf206558c15c5c0'
         ])->get("https://api.football-data.org/v4/competitions/$competition/matches");
 
+        $winner = $response['matches'][0]['score']['winner'];
+        $home_team = $response['matches'][0]['homeTeam']['name'];
+        $away_team = $response['matches'][0]['awayTeam']['name'];
+
+        $winner = $winner == 'AWAY_TEAM' ? $away_team : ($winner == 'HOME_TEAM' ? $home_team : 'Aún no publicado');
+
         return response()->view('matches', [
             'competition' => $response['matches'][0]['competition']['name'],
             'date' => $response['matches'][0]['season']['startDate'],
-            'home_team' => $response['matches'][0]['homeTeam']['name'],
-            'away_team' => $response['matches'][0]['awayTeam']['name'],
-            'winner' => $response['matches'][0]['score']['winner'] ? ?? 'Aún no publicado'
+            'home_team' => $home_team,
+            'away_team' => $away_team,
+            'winner' => $winner
         ]);
     }
 }
